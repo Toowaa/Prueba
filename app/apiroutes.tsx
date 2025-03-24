1e5faea329e4ef8afc52fb0de39a8303d38d54be
Toowaa <brahanax@gmail.com>
2025-03-23 23:22:03 -0500

    fix

--- app/apiroutes.tsx
+++ app/apiroutes.tsx
@@ -1,8 +1,9 @@
 import { Order } from "@/interface";
 
 export async function GetOrders(): Promise<Order[]> {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/order`);
+    const response = await fetch(`${process.env.NEXT_PUBLIC_API_URL}api/order`, {
+        cache: "no-store", 
+    });
     if (!response.ok) throw new Error("Error en la respuesta del servidor");
-    const data: Order[] = await response.json();
-    return data;
-  }
+    return response.json();
+}
